d6fcf2dfec3d0a4b7b9f028e96876681648e9198
Manos Surligas <surligas@csd.uoc.gr>
2014-11-19 19:04:36 +0200

    Improved examples

--- gr-cs436/examples/fm_receiver.py
+++ gr-cs436/examples/fm_receiver.py
@@ -2,7 +2,7 @@
 ##################################################
 # Gnuradio Python Flow Graph
 # Title: FM Receiver
-# Generated: Wed Nov 19 01:24:11 2014
+# Generated: Wed Nov 19 15:11:12 2014
 ##################################################
 
 from PyQt4 import Qt
@@ -52,42 +52,87 @@ class fm_receiver(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 4e6
-        self.nco_freq = nco_freq = 0
         self.lpf_decim = lpf_decim = 40
+        self.freq = freq = 0
         self.audio_samp_rate = audio_samp_rate = 96000
 
         ##################################################
         # Blocks
         ##################################################
-        self._nco_freq_layout = Qt.QVBoxLayout()
-        self._nco_freq_tool_bar = Qt.QToolBar(self)
-        self._nco_freq_layout.addWidget(self._nco_freq_tool_bar)
-        self._nco_freq_tool_bar.addWidget(Qt.QLabel("NCO Frequency"+": "))
+        self._freq_layout = Qt.QVBoxLayout()
+        self._freq_tool_bar = Qt.QToolBar(self)
+        self._freq_layout.addWidget(self._freq_tool_bar)
+        self._freq_tool_bar.addWidget(Qt.QLabel("Frequency"+": "))
         class qwt_counter_pyslot(Qwt.QwtCounter):
             def __init__(self, parent=None):
                 Qwt.QwtCounter.__init__(self, parent)
             @pyqtSlot('double')
             def setValue(self, value):
                 super(Qwt.QwtCounter, self).setValue(value)
-        self._nco_freq_counter = qwt_counter_pyslot()
-        self._nco_freq_counter.setRange(-2e6, 2e6, 1)
-        self._nco_freq_counter.setNumButtons(2)
-        self._nco_freq_counter.setValue(self.nco_freq)
-        self._nco_freq_tool_bar.addWidget(self._nco_freq_counter)
-        self._nco_freq_counter.valueChanged.connect(self.set_nco_freq)
-        self._nco_freq_slider = Qwt.QwtSlider(None, Qt.Qt.Horizontal, Qwt.QwtSlider.BottomScale, Qwt.QwtSlider.BgSlot)
-        self._nco_freq_slider.setRange(-2e6, 2e6, 1)
-        self._nco_freq_slider.setValue(self.nco_freq)
-        self._nco_freq_slider.setMinimumWidth(200)
-        self._nco_freq_slider.valueChanged.connect(self.set_nco_freq)
-        self._nco_freq_layout.addWidget(self._nco_freq_slider)
-        self.top_layout.addLayout(self._nco_freq_layout)
+        self._freq_counter = qwt_counter_pyslot()
+        self._freq_counter.setRange(-2e6, 2e6, 1)
+        self._freq_counter.setNumButtons(2)
+        self._freq_counter.setValue(self.freq)
+        self._freq_tool_bar.addWidget(self._freq_counter)
+        self._freq_counter.valueChanged.connect(self.set_freq)
+        self._freq_slider = Qwt.QwtSlider(None, Qt.Qt.Horizontal, Qwt.QwtSlider.BottomScale, Qwt.QwtSlider.BgSlot)
+        self._freq_slider.setRange(-2e6, 2e6, 1)
+        self._freq_slider.setValue(self.freq)
+        self._freq_slider.setMinimumWidth(200)
+        self._freq_slider.valueChanged.connect(self.set_freq)
+        self._freq_layout.addWidget(self._freq_slider)
+        self.top_layout.addLayout(self._freq_layout)
         self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
                 interpolation=int(samp_rate/lpf_decim/1000),
                 decimation=96,
                 taps=None,
                 fractional_bw=None,
         )
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
+        	1024, #size
+        	samp_rate, #samp_rate
+        	"", #name
+        	1 #number of inputs
+        )
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+        
+        self.qtgui_time_sink_x_0.set_y_label("Amplitude", "")
+        
+        self.qtgui_time_sink_x_0.enable_tags(-1, True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0.enable_grid(False)
+        
+        labels = ["", "", "", "", "",
+                  "", "", "", "", ""]
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "blue"]
+        styles = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+                   -1, -1, -1, -1, -1]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        
+        for i in xrange(2*1):
+            if len(labels[i]) == 0:
+                if(i % 2 == 0):
+                    self.qtgui_time_sink_x_0.set_line_label(i, "Re{{Data {0}}}".format(i/2))
+                else:
+                    self.qtgui_time_sink_x_0.set_line_label(i, "Im{{Data {0}}}".format(i/2))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+        
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
         self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
         	512, #size
         	firdes.WIN_BLACKMAN_hARRIS, #wintype
@@ -126,26 +171,35 @@ class fm_receiver(gr.top_block, Qt.QWidget):
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
         self.low_pass_filter_0 = filter.fir_filter_ccf(lpf_decim, firdes.low_pass(
-        	10, samp_rate, 100e3, 10e3, firdes.WIN_HAMMING, 6.76))
+        	1, samp_rate, 100e3, 10e3, firdes.WIN_HAMMING, 6.76))
         self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
+        self.blocks_multiply_const_vxx_0_0 = blocks.multiply_const_vff((-1, ))
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vff((1, ))
+        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
         self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, "/home/surligas/radio_capture_complex_96MHz.dat", True)
         self.audio_sink_0 = audio.sink(audio_samp_rate, "", True)
         self.analog_wfm_rcv_0 = analog.wfm_rcv(
         	quad_rate=samp_rate/lpf_decim,
         	audio_decimation=1,
         )
-        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, nco_freq, 1, 0)
+        self.analog_sig_source_x_0_0 = analog.sig_source_f(samp_rate, analog.GR_SIN_WAVE, freq, 1, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_f(4e6, analog.GR_COS_WAVE, freq, 1, 0)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_wfm_rcv_0, 0), (self.rational_resampler_xxx_0, 0))
-        self.connect((self.blocks_file_source_0, 0), (self.blocks_multiply_xx_0, 0))
-        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))
-        self.connect((self.blocks_multiply_xx_0, 0), (self.low_pass_filter_0, 0))
-        self.connect((self.blocks_multiply_xx_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.rational_resampler_xxx_0, 0), (self.audio_sink_0, 0))
         self.connect((self.low_pass_filter_0, 0), (self.analog_wfm_rcv_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.audio_sink_0, 0))
+        self.connect((self.analog_wfm_rcv_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.blocks_float_to_complex_0, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_float_to_complex_0, 1))
+        self.connect((self.blocks_float_to_complex_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_const_vxx_0_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0_0, 0), (self.blocks_float_to_complex_0, 0))
 
 
     def closeEvent(self, event):
@@ -158,24 +212,26 @@ class fm_receiver(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
         self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
-        self.low_pass_filter_0.set_taps(firdes.low_pass(10, self.samp_rate, 100e3, 10e3, firdes.WIN_HAMMING, 6.76))
-
-    def get_nco_freq(self):
-        return self.nco_freq
-
-    def set_nco_freq(self, nco_freq):
-        self.nco_freq = nco_freq
-        self.analog_sig_source_x_0.set_frequency(self.nco_freq)
-        Qt.QMetaObject.invokeMethod(self._nco_freq_counter, "setValue", Qt.Q_ARG("double", self.nco_freq))
-        Qt.QMetaObject.invokeMethod(self._nco_freq_slider, "setValue", Qt.Q_ARG("double", self.nco_freq))
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 100e3, 10e3, firdes.WIN_HAMMING, 6.76))
+        self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
     def get_lpf_decim(self):
         return self.lpf_decim
 
     def set_lpf_decim(self, lpf_decim):
         self.lpf_decim = lpf_decim
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        Qt.QMetaObject.invokeMethod(self._freq_counter, "setValue", Qt.Q_ARG("double", self.freq))
+        Qt.QMetaObject.invokeMethod(self._freq_slider, "setValue", Qt.Q_ARG("double", self.freq))
+        self.analog_sig_source_x_0_0.set_frequency(self.freq)
+        self.analog_sig_source_x_0.set_frequency(self.freq)
 
     def get_audio_samp_rate(self):
         return self.audio_samp_rate
--- gr-cs436/examples/top_block.py
+++ gr-cs436/examples/top_block.py
@@ -2,14 +2,13 @@
 ##################################################
 # Gnuradio Python Flow Graph
 # Title: Top Block
-# Generated: Wed Nov 19 00:19:58 2014
+# Generated: Wed Nov 19 15:25:10 2014
 ##################################################
 
 from PyQt4 import Qt
 from PyQt4.QtCore import QObject, pyqtSlot
 from gnuradio import analog
 from gnuradio import blocks
-from gnuradio import cs436
 from gnuradio import eng_notation
 from gnuradio import gr
 from gnuradio import qtgui
@@ -50,136 +49,92 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 2e6
-        self.real_mult = real_mult = 1
-        self.imag_mult = imag_mult = 1
-        self.freq = freq = 1e3
+        self.vlen = vlen = 16
+        self.samp_rate = samp_rate = 1e6
+        self.amp = amp = 0
 
         ##################################################
         # Blocks
         ##################################################
-        self._real_mult_layout = Qt.QVBoxLayout()
-        self._real_mult_tool_bar = Qt.QToolBar(self)
-        self._real_mult_layout.addWidget(self._real_mult_tool_bar)
-        self._real_mult_tool_bar.addWidget(Qt.QLabel("Real multiplier"+": "))
+        self._amp_layout = Qt.QVBoxLayout()
+        self._amp_tool_bar = Qt.QToolBar(self)
+        self._amp_layout.addWidget(self._amp_tool_bar)
+        self._amp_tool_bar.addWidget(Qt.QLabel("Noise Power"+": "))
         class qwt_counter_pyslot(Qwt.QwtCounter):
             def __init__(self, parent=None):
                 Qwt.QwtCounter.__init__(self, parent)
             @pyqtSlot('double')
             def setValue(self, value):
                 super(Qwt.QwtCounter, self).setValue(value)
-        self._real_mult_counter = qwt_counter_pyslot()
-        self._real_mult_counter.setRange(0, 20, 1)
-        self._real_mult_counter.setNumButtons(2)
-        self._real_mult_counter.setValue(self.real_mult)
-        self._real_mult_tool_bar.addWidget(self._real_mult_counter)
-        self._real_mult_counter.valueChanged.connect(self.set_real_mult)
-        self._real_mult_slider = Qwt.QwtSlider(None, Qt.Qt.Horizontal, Qwt.QwtSlider.BottomScale, Qwt.QwtSlider.BgSlot)
-        self._real_mult_slider.setRange(0, 20, 1)
-        self._real_mult_slider.setValue(self.real_mult)
-        self._real_mult_slider.setMinimumWidth(200)
-        self._real_mult_slider.valueChanged.connect(self.set_real_mult)
-        self._real_mult_layout.addWidget(self._real_mult_slider)
-        self.top_layout.addLayout(self._real_mult_layout)
-        self._imag_mult_layout = Qt.QVBoxLayout()
-        self._imag_mult_tool_bar = Qt.QToolBar(self)
-        self._imag_mult_layout.addWidget(self._imag_mult_tool_bar)
-        self._imag_mult_tool_bar.addWidget(Qt.QLabel("Imaginary multiplier"+": "))
-        class qwt_counter_pyslot(Qwt.QwtCounter):
-            def __init__(self, parent=None):
-                Qwt.QwtCounter.__init__(self, parent)
-            @pyqtSlot('double')
-            def setValue(self, value):
-                super(Qwt.QwtCounter, self).setValue(value)
-        self._imag_mult_counter = qwt_counter_pyslot()
-        self._imag_mult_counter.setRange(0, 20, 1)
-        self._imag_mult_counter.setNumButtons(2)
-        self._imag_mult_counter.setValue(self.imag_mult)
-        self._imag_mult_tool_bar.addWidget(self._imag_mult_counter)
-        self._imag_mult_counter.valueChanged.connect(self.set_imag_mult)
-        self._imag_mult_slider = Qwt.QwtSlider(None, Qt.Qt.Horizontal, Qwt.QwtSlider.BottomScale, Qwt.QwtSlider.BgSlot)
-        self._imag_mult_slider.setRange(0, 20, 1)
-        self._imag_mult_slider.setValue(self.imag_mult)
-        self._imag_mult_slider.setMinimumWidth(200)
-        self._imag_mult_slider.valueChanged.connect(self.set_imag_mult)
-        self._imag_mult_layout.addWidget(self._imag_mult_slider)
-        self.top_layout.addLayout(self._imag_mult_layout)
-        self._freq_layout = Qt.QVBoxLayout()
-        self._freq_tool_bar = Qt.QToolBar(self)
-        self._freq_layout.addWidget(self._freq_tool_bar)
-        self._freq_tool_bar.addWidget(Qt.QLabel("Frequency"+": "))
-        class qwt_counter_pyslot(Qwt.QwtCounter):
-            def __init__(self, parent=None):
-                Qwt.QwtCounter.__init__(self, parent)
-            @pyqtSlot('double')
-            def setValue(self, value):
-                super(Qwt.QwtCounter, self).setValue(value)
-        self._freq_counter = qwt_counter_pyslot()
-        self._freq_counter.setRange(500, 100e3, 1)
-        self._freq_counter.setNumButtons(2)
-        self._freq_counter.setValue(self.freq)
-        self._freq_tool_bar.addWidget(self._freq_counter)
-        self._freq_counter.valueChanged.connect(self.set_freq)
-        self._freq_slider = Qwt.QwtSlider(None, Qt.Qt.Horizontal, Qwt.QwtSlider.BottomScale, Qwt.QwtSlider.BgSlot)
-        self._freq_slider.setRange(500, 100e3, 1)
-        self._freq_slider.setValue(self.freq)
-        self._freq_slider.setMinimumWidth(200)
-        self._freq_slider.valueChanged.connect(self.set_freq)
-        self._freq_layout.addWidget(self._freq_slider)
-        self.top_layout.addLayout(self._freq_layout)
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+        self._amp_counter = qwt_counter_pyslot()
+        self._amp_counter.setRange(0, 1, 0.01)
+        self._amp_counter.setNumButtons(2)
+        self._amp_counter.setValue(self.amp)
+        self._amp_tool_bar.addWidget(self._amp_counter)
+        self._amp_counter.valueChanged.connect(self.set_amp)
+        self._amp_slider = Qwt.QwtSlider(None, Qt.Qt.Horizontal, Qwt.QwtSlider.BottomScale, Qwt.QwtSlider.BgSlot)
+        self._amp_slider.setRange(0, 1, 0.01)
+        self._amp_slider.setValue(self.amp)
+        self._amp_slider.setMinimumWidth(200)
+        self._amp_slider.valueChanged.connect(self.set_amp)
+        self._amp_layout.addWidget(self._amp_slider)
+        self.top_layout.addLayout(self._amp_layout)
+        self.qtgui_const_sink_x_0 = qtgui.const_sink_c(
         	1024, #size
-        	samp_rate, #samp_rate
-        	"", #name
+        	"Constellation Plot", #name
         	2 #number of inputs
         )
-        self.qtgui_time_sink_x_0.set_update_time(0.10)
-        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+        self.qtgui_const_sink_x_0.set_update_time(0.10)
+        self.qtgui_const_sink_x_0.set_y_axis(-1, 1)
+        self.qtgui_const_sink_x_0.set_x_axis(-1, 1)
+        self.qtgui_const_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, "")
+        self.qtgui_const_sink_x_0.enable_autoscale(False)
+        self.qtgui_const_sink_x_0.enable_grid(True)
         
-        self.qtgui_time_sink_x_0.set_y_label("Amplitude", "")
-        
-        self.qtgui_time_sink_x_0.enable_tags(-1, True)
-        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_AUTO, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0.enable_autoscale(True)
-        self.qtgui_time_sink_x_0.enable_grid(True)
-        
-        labels = ["Real part", "Imaginary part", "", "", "",
+        labels = ["Reference", "With noise", "", "", "",
                   "", "", "", "", ""]
-        widths = [1, 1, 1, 1, 1,
+        widths = [2, 1, 1, 1, 1,
                   1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-                  "magenta", "yellow", "dark red", "dark green", "blue"]
-        styles = [1, 1, 1, 1, 1,
-                  1, 1, 1, 1, 1]
-        markers = [-1, -1, -1, -1, -1,
-                   -1, -1, -1, -1, -1]
+        colors = ["blue", "red", "red", "red", "red",
+                  "red", "red", "red", "red", "red"]
+        styles = [0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0]
+        markers = [0, 0, 0, 0, 0,
+                   0, 0, 0, 0, 0]
         alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
                   1.0, 1.0, 1.0, 1.0, 1.0]
-        
         for i in xrange(2):
             if len(labels[i]) == 0:
-                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+                self.qtgui_const_sink_x_0.set_line_label(i, "Data {0}".format(i))
             else:
-                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
-            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
-            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+                self.qtgui_const_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_const_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_const_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_const_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_const_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_const_sink_x_0.set_line_alpha(i, alphas[i])
         
-        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.cs436_hello_world_sync_block_0 = cs436.hello_world_sync_block(real_mult, imag_mult)
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
-        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, freq, 1, 0)
+        self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_const_sink_x_0_win)
+        self.blocks_vector_to_stream_0_0 = blocks.vector_to_stream(gr.sizeof_gr_complex*1, vlen)
+        self.blocks_vector_to_stream_0 = blocks.vector_to_stream(gr.sizeof_gr_complex*1, vlen)
+        self.blocks_vector_source_x_0_0 = blocks.vector_source_c([1+0j, -1+0j]*8, True, 16, [])
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*vlen, samp_rate,True)
+        self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, vlen)
+        self.blocks_add_xx_0 = blocks.add_vcc(vlen)
+        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, amp, 0)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.cs436_hello_world_sync_block_0, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.cs436_hello_world_sync_block_0, 0))
-        self.connect((self.cs436_hello_world_sync_block_0, 1), (self.qtgui_time_sink_x_0, 1))
+        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_stream_to_vector_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_add_xx_0, 0))
+        self.connect((self.blocks_stream_to_vector_0, 0), (self.blocks_add_xx_0, 1))
+        self.connect((self.blocks_vector_to_stream_0, 0), (self.qtgui_const_sink_x_0, 0))
+        self.connect((self.blocks_add_xx_0, 0), (self.blocks_vector_to_stream_0_0, 0))
+        self.connect((self.blocks_vector_to_stream_0_0, 0), (self.qtgui_const_sink_x_0, 1))
+        self.connect((self.blocks_vector_source_x_0_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.blocks_vector_source_x_0_0, 0), (self.blocks_vector_to_stream_0, 0))
 
 
     def closeEvent(self, event):
@@ -187,41 +142,27 @@ class top_block(gr.top_block, Qt.QWidget):
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
 
+    def get_vlen(self):
+        return self.vlen
+
+    def set_vlen(self, vlen):
+        self.vlen = vlen
+
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
-        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
-    def get_real_mult(self):
-        return self.real_mult
+    def get_amp(self):
+        return self.amp
 
-    def set_real_mult(self, real_mult):
-        self.real_mult = real_mult
-        self.cs436_hello_world_sync_block_0.set_real_multiplier(self.real_mult)
-        Qt.QMetaObject.invokeMethod(self._real_mult_counter, "setValue", Qt.Q_ARG("double", self.real_mult))
-        Qt.QMetaObject.invokeMethod(self._real_mult_slider, "setValue", Qt.Q_ARG("double", self.real_mult))
-
-    def get_imag_mult(self):
-        return self.imag_mult
-
-    def set_imag_mult(self, imag_mult):
-        self.imag_mult = imag_mult
-        self.cs436_hello_world_sync_block_0.set_imag_multiplier(self.imag_mult)
-        Qt.QMetaObject.invokeMethod(self._imag_mult_counter, "setValue", Qt.Q_ARG("double", self.imag_mult))
-        Qt.QMetaObject.invokeMethod(self._imag_mult_slider, "setValue", Qt.Q_ARG("double", self.imag_mult))
-
-    def get_freq(self):
-        return self.freq
-
-    def set_freq(self, freq):
-        self.freq = freq
-        Qt.QMetaObject.invokeMethod(self._freq_counter, "setValue", Qt.Q_ARG("double", self.freq))
-        Qt.QMetaObject.invokeMethod(self._freq_slider, "setValue", Qt.Q_ARG("double", self.freq))
-        self.analog_sig_source_x_0.set_frequency(self.freq)
+    def set_amp(self, amp):
+        self.amp = amp
+        self.analog_noise_source_x_0.set_amplitude(self.amp)
+        Qt.QMetaObject.invokeMethod(self._amp_counter, "setValue", Qt.Q_ARG("double", self.amp))
+        Qt.QMetaObject.invokeMethod(self._amp_slider, "setValue", Qt.Q_ARG("double", self.amp))
 
 if __name__ == '__main__':
     import ctypes
